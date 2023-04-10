963932a66659cda839e50e2b486c8d4407a2738b
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-04-10 17:27:53 -0300

    versao finalizada sem o bonus

--- src/app.js
+++ src/app.js
@@ -9,20 +9,22 @@ let usersServer = [];
 let tweetsServer = [];
 
 app.post("/sign-up", (req, res) => {
-  const { usersname, avatar } = req.body;
-  const user = { usersname, avatar };
+  const { username, avatar } = req.body;
+  const user = { username, avatar };
 
   usersServer.push(user);
   res.send("OK");
 });
 
-app.post("/tweet", (req, res) => {
-  const checkUser = usersServer.find((u) => u.usersname === usersname);
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+
+  const checkUser = usersServer.find((u) => u.username === username);
   if (!checkUser) {
     res.send("UNIAUTHORIZED");
   }
-  const { usersname, tweet } = req.body;
-  const tweets = { usersname, tweet };
+
+  const tweets = { username, tweet };
 
   tweetsServer.push(tweets);
   res.send("OK");
@@ -34,10 +36,11 @@ app.get("/tweets", (req, res) => {
     res.send([]);
   }
   const tweets = lastTenTweets.map((t) => {
-    const user = usersServer.find((u) => u.usersname === t.username);
+    const user = usersServer.find((u) => u.username === t.username);
+
     return {
       username: t.username,
-      avatar: user ? user.avatar : null,
+      avatar: user.avatar,
       tweet: t.tweet,
     };
   });
